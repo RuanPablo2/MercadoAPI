34bf048634b8b897e4bb9fbafcb304530477ed42
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-02-01 09:41:09 -0300

    Project created

---
+++ src/test/java/com/RuanPablo2/mercadoapi/MercadoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RuanPablo2.mercadoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
